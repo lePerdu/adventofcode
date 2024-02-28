421348b9894127789e2cf2240083ddcf9a51d2ea
lePerdu <zdpeltzer@gmail.com>
2024-02-28 13:02:12 -0500

    Flush output when printing answers

--- lib/Main.ml
+++ lib/Main.ml
@@ -1,4 +1,4 @@
 let run input_file read_input part1 part2 =
   let input = read_input input_file in
-  Printf.printf "Part1: %d\n" (part1 input);
-  Printf.printf "Part2: %d\n" (part2 input)
+  Printf.printf "Part1: %d\n%!" (part1 input);
+  Printf.printf "Part2: %d\n%!" (part2 input)
