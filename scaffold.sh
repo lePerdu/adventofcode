e7513beb9c6b8d0c18ea1782b88f3a69f44fd495
lePerdu <zdpeltzer@gmail.com>
2024-02-28 13:02:03 -0500

    Fix scaffold to add new lines

--- scaffold.sh
+++ scaffold.sh
@@ -8,5 +8,5 @@ fi
 cd $(dirname "$0")
 
 n=$1
-sed -i "s/(names \(.*\))/(names \1 day$n)/" bin/dune
+sed -i "s/\(day.*\))/\1\n  day$n)/" bin/dune
 touch input/day$n.txt input/day$n-example.txt bin/day$n.ml
