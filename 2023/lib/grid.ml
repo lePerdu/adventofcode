ccbc75589d83d73b991b8ed4c5dfb3490c5349b8
lePerdu <zdpeltzer@gmail.com>
2024-10-01 13:11:21 -0400

    2023: Add day 21

--- 2023/lib/grid.ml
+++ 2023/lib/grid.ml
@@ -4,6 +4,16 @@ let make row_count col_count fill =
   { data = Array.make_matrix row_count col_count fill }
 
 let map f g = { data = Array.map (Array.map f) g.data }
+
+let mapi f g =
+  {
+    data =
+      Array.mapi
+        (fun rowi ->
+          Array.mapi (fun coli cell -> f { Coord.row = rowi; col = coli } cell))
+        g.data;
+  }
+
 let copy g = map Fun.id g
 let rows g = Array.length g.data
 let cols g = Array.length g.data.(0)
@@ -11,6 +21,7 @@ let cols g = Array.length g.data.(0)
 let in_bounds g ({ row; col } : Coord.t) =
   0 <= row && row < rows g && 0 <= col && col < cols g
 
+let size g : Coord.t = { row = rows g; col = cols g }
 let max_coord g : Coord.t = { row = rows g - 1; col = cols g - 1 }
 let get g ({ row; col } : Coord.t) = g.data.(row).(col)
 let set g ({ row; col } : Coord.t) v = g.data.(row).(col) <- v
@@ -52,6 +63,8 @@ let input of_char ch =
       in
       { data = rows }
   | None -> (* Only raise if the first line is missing *) raise End_of_file
+
+let input_file of_char path = In_channel.with_open_text path (input of_char)
 
 let output to_char ch g =
   for row = 0 to rows g - 1 do
